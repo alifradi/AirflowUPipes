b751e2606c591051236cf9a70e2fac46d3c525c1
alifradi <alifradi7@gmail.com>
2025-06-05 23:25:32 -0400

    both pipes work fine to get data sources

--- data_getters/raw_getters.py
+++ data_getters/raw_getters.py
@@ -109,8 +109,103 @@ MTL_MAP = {
     'mtl_fines_food': {
         'resource_id': '7f939a08-be8a-45e1-b208-d8744dca8fc6',
         'has_coords': False,
-        'category': 'alimentaires'}
+        'category': 'alimentaires'
+    },
+    'mtl_index_quality_life': {
+        'resource_id': '0f0f8c6a-b503-4565-9583-d4f21db9e6fe',
+        'has_coords': False
+    },
+    'mtl_Residential_fines': {
+        'resource_id': 'f270cb02-ca30-4b3b-96eb-f0dbdbc50ea7',
+        'has_coords': False,
+        'description': 'Infractions au Règlement sur la salubrité des logements'
+    },
+    'mtl_business': {
+        'resource_id': 'ceee5da3-56ba-40f4-9008-8678bf4c14fe',
+        'has_coords': False,
+        'description': 'Fournisseurs de la ville de Montréal'
+    },
+    'mtl_Residential_properties': {
+        'resource_id': '2b9dfc3d-91d3-48de-b32c-a2a6d9417079',
+        'has_coords': False
+    },
+    'mtl_green_transportation': {
+        'resource_id': '04e04cd7-96e3-4aed-bc5a-d30dc08c08fc',
+        'has_coords': False
+    },
+    'mtl_financial_help': {
+        'resource_id': '5a2bef39-3b3e-4d7d-82cf-658f7821e141',
+        'has_coords': False
+    },
+    'mtl_parking_fines': {
+        'resource_id': 'e7df09fb-af5a-476f-861e-ea23777749b5',
+        'has_coords': False
+    },
+    'mtl_ardm': {
+        'resource_id': '87af3a62-ee9a-40ad-b7d9-517ab3f12fad',
+        'has_coords': False
+    },
+    'mtl_uadm': {
+        'resource_id': '61a4428e-804b-4022-8f1a-8e40c9a46cb1',
+        'has_coords': False
+    },
+    'mtl_Municipalities': {
+        'resource_id': '19385b4e-5503-4330-9e59-f998f5918363',
+        'has_coords': False
+    },
+    'mtl_parc': {
+        'resource_id': '4731b64f-29cc-4e08-bc44-8752ae2fcafb',
+        'has_coords': True
+    },
+    'mtl_crimes': {
+        'resource_id': 'c6f482bf-bf0f-4960-8b2f-9982c211addd',
+        'has_coords': True
+    },
+    'mtl_bus': {
+        'resource_id': '59f204f7-b09d-4636-988a-bbe8d3b869b1',
+        'has_coords': True
+    },
+    'mtl_Bugs': {
+        'resource_id': 'ba28703e-ce85-4293-8a37-88932bf4ae93',
+        'has_coords': True
+    },
+    'mtl_commercial_sites': {
+        'resource_id': 'fb2e534a-c573-45b5-b62b-8f99e3a37cd1',
+        'has_coords': True
+    },
+    'mtl_food_establishments': {
+        'resource_id': '28a4957d-732e-48f9-8adb-0624867d9bb0',
+        'has_coords': True
+    },
+    'mtl_fire_fights': {
+        'resource_id': '71e86320-e35c-4b4c-878a-e52124294355',
+        'has_coords': True
+    },
+    'mtl_public_sites': {
+        'resource_id': '4731b64f-29cc-4e08-bc44-8752ae2fcafb',
+        'has_coords': True
+    },
+    'mtl_retailer_construction_permit': {
+        'resource_id': '5232a72d-235a-48eb-ae20-bb9d501300ad',
+        'has_coords': True
+    },
+    'mtl_police_offices': {
+        'resource_id': 'c9f296dd-596e-48ed-9c76-37230b2c916d',
+        'has_coords': True
+    },
+    'mtl_Passenger_count': {
+        'resource_id': 'f82f00c0-baed-4fa1-8b01-6ed60146d102',
+        'has_coords': True
+    },
+    'mtl_Population': {
+        'resource_id': '604cabd1-891d-4327-a85c-5991dccc82d4',
+        'has_coords': False
+    },
+    'mtl_Rich_distribution': {
+        'resource_id': '9744f904-6519-4df0-8b0a-8dd4806f619e',
+        'has_coords': False
     }
+}
 
 def fetch_and_store_ontario_dataset(
     package_id: str,
@@ -167,12 +262,15 @@ def fetch_and_store_ontario_dataset(
             col_order = [col for col in col_order if col in df.columns]
             df = df[col_order]
         # Cast all columns to string (text)
+        # Convert all columns to string before writing
         df = df.astype(str).replace({"nan": np.nan, "None": np.nan})
+        df = df.applymap(lambda x: str(x) if pd.notna(x) else None)
         df.to_csv(csv_path, encoding='utf-8-sig', index=False)
         if connection and table_name:
             logger.info(f"Writing to Postgres in chunks: {table_name}")
             for chunk in pd.read_csv(csv_path, chunksize=chunk_size, dtype=str):
-                chunk = chunk.astype(str).replace({"nan": np.nan, "None": np.nan})
+                # Ensure all columns are strings and handle NaN values
+                chunk = chunk.applymap(lambda x: str(x) if pd.notna(x) else None)
                 chunk.to_sql(table_name, connection, if_exists='append', index=False, schema=schema_name, method='multi', chunksize=1000)
                 total_rows += len(chunk)
         else:
@@ -185,9 +283,172 @@ def fetch_and_store_ontario_dataset(
 
 
 TO_MAP = {
-   
+    'to_neighbourhood': {
+        'resource_id': 'neighbourhood-profiles',
+        'has_coords': False
+    },
      'to_Business': {
         'resource_id': 'business-improvement-areas',
+        'has_coords': False
+    },
+     'to_Business_permits': {
+        'resource_id': 'municipal-licensing-and-standards-business-licences-and-permits',
+        'has_coords': False
+    },
+     'to_fines_food': {
+        'resource_id': 'dinesafe',
+        'has_coords': False
+    },
+     'to_food_establishments': {
+        'resource_id': 'cafeto-curb-lane-parklet-cafe-locations',
+        'has_coords': False
+    },
+     'to_transportation_cycle': {
+        'resource_id': 'bicycle-parking-racks',
+        'has_coords': False
+    },
+     'to_crimes': {
+        'resource_id': 'neighbourhood-crime-rates',
+        'has_coords': False
+    },
+     'to_short_rentals': {
+        'resource_id': 'short-term-rentals-registration',
+        'has_coords': False
+    },
+     'to_sites_opportunities': {
+        'resource_id': 'toronto-signature-sites',
+        'has_coords': False
+    },
+     'to_permits': {
+        'resource_id': 'building-permits-cleared-permits',
+        'has_coords': False
+    },
+     'to_fines_buidlings': {
+        'resource_id': 'apartment-building-evaluation',
+        'has_coords': False
+    },
+     'to_public_solar_pannels': {
+        'resource_id': 'renewable-energy-installations',
+        'has_coords': False
+    },
+     'to_private_solar_pannels': {
+        'resource_id': 'solarto',
+        'has_coords': False
+    },
+     'to_Bids_Solicitations': {
+        'resource_id': 'tobids-all-open-solicitations',
+        'has_coords': False
+    },
+     'to_barbers_inspections': {
+        'resource_id': 'bodysafe',
+        'has_coords': False
+    },
+     'to_residential_properties': {
+        'resource_id': 'apartment-building-registration',
+        'has_coords': False
+    },
+     'to_health_casualties': {
+        'resource_id': 'wellbeing-toronto-health',
+        'has_coords': False
+    },
+     'to_ward_profiling': {
+        'resource_id': 'ward-profiles-25-ward-model',
+        'has_coords': False
+    },
+     'to_health_outbreaks': {
+        'resource_id': 'outbreaks-in-toronto-healthcare-institutions',
+        'has_coords': False
+    },
+     'to_labor_force': {
+        'resource_id': 'labour-force-survey',
+        'has_coords': False
+    },
+     'to_Building_permits_signs': {
+        'resource_id': 'building-permits-signs',
+        'has_coords': False
+    },
+     'to_Street_tree': {
+        'resource_id': 'street-tree-data',
+        'has_coords': False
+    },
+     'to_KPIS': {
+        'resource_id': 'toronto-s-dashboard-key-indicators',
+        'has_coords': False
+    },
+     'to_cycle_parking': {
+        'resource_id': 'bicycle-parking-bike-stations-indoor',
+        'has_coords': False
+    },
+     'to_neighjbourhood_housing_density': {
+        'resource_id': 'social-housing-unit-density-by-neighbourhoods',
+        'has_coords': False
+    },
+     'to_employment': {
+        'resource_id': 'toronto-employment-survey-summary-tables',
+        'has_coords': False
+    },
+     'to_real_estate_inventory': {
+        'resource_id': 'real-estate-asset-inventory',
+        'has_coords': False
+    },
+     'to_parking_fines': {
+        'resource_id': 'parking-tickets',
         'has_coords': True
+    },
+     'to_building_active_permits': {
+        'resource_id': 'building-permits-active-permits',
+        'has_coords': False
+    },
+     'to_fire_fights': {
+        'resource_id': 'fire-incidents',
+        'has_coords': False
+    },
+     'to_parking_lots_facilities': {
+        'resource_id': 'parking-lot-facilities',
+        'has_coords': True
+    },
+     'to_noisy_places': {
+        'resource_id': 'noise-exemption-permits',
+        'has_coords': False
+    },
+     'to_wards': {
+        'resource_id': 'city-wards',
+        'has_coords': False
+    },
+     'to_on_street_permit_parking_area_maps': {
+        'resource_id': 'on-street-permit-parking-area-maps',
+        'has_coords': False
+    },
+     'to_property_boundaries': {
+        'resource_id': 'property-boundaries',
+        'has_coords': False
+    },
+     'to_green_energy_installations': {
+        'resource_id': 'renewable-energy-installations',
+        'has_coords': False
+    },
+     'to_non_competitive_contracts': {
+        'resource_id': 'tobids-non-competitive-contracts',
+        'has_coords': False
+    },
+     'to_fire_inspections': {
+        'resource_id': 'highrise-residential-fire-inspection-results',
+        'has_coords': False
+    },
+     'to_green_roofs': {
+        'resource_id': 'building-permits-green-roofs',
+        'has_coords': False
+    },
+     'to_mappings': {
+        'resource_id': 'web-map-services',
+        'has_coords': False
+    },
+     'to_street_routes': {
+        'resource_id': 'ttc-routes-and-schedules',
+        'has_coords': False
+    },
+     'to_apt_registrations': {
+        'resource_id': 'apartment-building-registration',
+        'has_coords': False
     }
 }
